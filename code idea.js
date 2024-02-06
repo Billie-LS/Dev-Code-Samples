e32b4ffbd6afb506b610203d5138d8cf8597c96d
Billie-LS <lskylizard@yahoo.com>
2024-02-06 15:23:31 -0600

    sample code question

---
+++ code idea.js
@@ -0,0 +1,8 @@
+/*
+there is a long room (corridor) with 100 lights along the wall and corresponding toggle switches
+initially all the lights were off
+at the two ends of the room is entry and exit doors
+then 100 people entered the room, and walked along the corridor and exited
+in that process they pressed every n-th switch where n is the person's order of entry
+initially the array will be [false, false, false,... ] all false - 100 elements
+*/
